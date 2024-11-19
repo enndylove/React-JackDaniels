97d6750becf4d0f8f6e080f97e18567eda2cce3f
enndy <91780685+enndylove@users.noreply.github.com>
2024-11-19 19:36:53 +0200

    Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import './index.scss';
+
+import logo from './images/logo.svg'
+import IconBasket from './images/basket.svg'
+
+const Header = ({date}) => {
+    return (
+        <header className="header m-auto">
+            <div className="header__block first flex justify-between items-center">
+                <div className="header__width-wrapp"></div>
+
+                <img
+                    className="header__logo"
+                    src={logo}
+                    alt="logo"
+                />
+
+                <img
+                    className="header__basket cursor-pointer header__width-wrapp"
+                    src={IconBasket}
+                    alt="basket-logo"
+                />
+            </div>
+
+            <div className="header__block second flex justify-between items-center">
+                <span className="header__block-text">
+                    Loyalty program
+                </span>
+
+                <span className="header__block-text">
+                    {date}
+                </span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
