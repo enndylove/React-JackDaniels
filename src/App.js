3e4878cf1fc78366b7cfd5026be3d003ae67a6ff
enndy <91780685+enndylove@users.noreply.github.com>
2024-11-19 19:37:34 +0200

    use components in Main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import {lazy} from "react";
+
+const ActionTime = lazy(() => import("./components/ActionTime"));
+const Header = lazy(() => import("./components/Header"));
+
 function App() {
   return (
     <div className="App">
+      <ActionTime
+          minutes="1"
+          seconds="20"
+      />
+      <Header
+          date="2 June 2024"
+      />
     </div>
   );
 }
